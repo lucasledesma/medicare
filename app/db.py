a00daedaf49e1982d0252906b6f19fae8eb4843b
Lucas Ledesma <lucas.ledesma@accuv.com>
2020-11-02 18:38:20 -0300

    Maybe not this way

--- app/db.py
+++ app/db.py
@@ -1,7 +1,9 @@
 import databases
-from sqlalchemy import Column, Integer, Table, MetaData, String
+from sqlalchemy import Column, Integer, Table, MetaData, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy
+
 
 Base = declarative_base()
 
@@ -11,30 +13,37 @@ database = databases.Database(DATABASE_URL)
 
 metadata = MetaData()
 
-providers = Table(
-    "providers",
-    metadata,
-    Column(name="id", type_=Integer, primary_key=True),
-    Column(name="lastname", type_=sqlalchemy.String),
-    Column(name="firstname", type_=sqlalchemy.String),
-    Column(name="middle_initial", type_=sqlalchemy.String),
-    Column(name="credentials" , type_=sqlalchemy.String),
-    Column(name="gender" , type_=sqlalchemy.String),
-    Column(name="entity_type" , type_=sqlalchemy.String),
-    Column(name='street_address_1', type_=sqlalchemy.String),
-    Column(name="street_address_2" , type_=sqlalchemy.String),
-    Column(name="city" , type_=sqlalchemy.String),
-    Column(name="zip_code" , type_=sqlalchemy.String),
-    Column(name="state_code" , type_=sqlalchemy.String),
-    Column(name="country_code" , type_=sqlalchemy.String),
-    Column(name="provider_type" , type_=sqlalchemy.String),
-    Column(name="medicare_participation_indicator", type_=sqlalchemy.String),
+class ProviderTable(Base):
+    __tablename__ = 'providers'
+    id = Column(name="id", type_=Integer, primary_key=True)
+    lastname = Column(name="lastname", type_=sqlalchemy.String)
+    firstname = Column(name="firstname", type_=sqlalchemy.String)
+    middle_initial = Column(name="middle_initial", type_=sqlalchemy.String)
+    data = relationship("MedicareDataTable")
+    credentials = Column(name="credentials" , type_=sqlalchemy.String)
+    # Column(name="gender" , type_=sqlalchemy.String)
+    # Column(name="entity_type" , type_=sqlalchemy.String)
+    # Column(name='street_address_1', type_=sqlalchemy.String)
+    # Column(name="street_address_2" , type_=sqlalchemy.String)
+    # Column(name="city" , type_=sqlalchemy.String)
+    # Column(name="zip_code" , type_=sqlalchemy.String)
+    # Column(name="state_code" , type_=sqlalchemy.String)
+    # Column(name="country_code" , type_=sqlalchemy.String)
+    # Column(name="provider_type" , type_=sqlalchemy.String)
+    # Column(name="medicare_participation_indicator", type_=sqlalchemy.String)
     #Column(name="hcpcs_code", type_=sqlalchemy.String)
 
-)
+# )
+
+class MedicareDataTable(Base):
+    __tablename__ = 'medicare_data'
+    id =  Column(name="id", type_=Integer, primary_key=True)
+    provider_id = Column(Integer, ForeignKey('ProviderTable.id'))
+    parent = relationship("ProviderTable", back_populates="data")
 
 engine = sqlalchemy.create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 metadata.create_all(engine)
+
--- app/service.py
+++ app/service.py
@@ -1,9 +1,12 @@
-from db import database,providers
+from db import database,ProviderTable,MedicareDataTable
+from sqlalchemy.sql import select
 
 async def get_providers(skip: int = 0, take: int = 20):
-    query = providers.select().offset(skip).limit(take)
+    query = ProviderTable.select().offset(skip).limit(take)
     return await database.fetch_all(query)    
 
 async def get_provider(provider_id: int):
-    query = providers.select().where(providers.c.id == provider_id)
+    # query = database.query(ProviderTable).filter(ProviderTable.c.id == provider_id)
+    query = select([ProviderTable]).where(ProviderTable.id == provider_id)
+    print(await database.fetch_one(query))
     return await database.fetch_one(query)
